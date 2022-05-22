b950e6b061bbd755e3c02d44ed5547e816e144d4
eroquel <eroquel@users.noreply.github.com>
2022-05-22 22:27:25 +0000

    deploy: 4dc5cf65cfb298820a5afe71bc25d794e8436105

---
+++ _dist_/index.js
@@ -0,0 +1,30 @@
+import { registerImage } from "./lazy.js";
+
+const maximum = 122;
+const minimum = 0;
+
+const random = () => Math.floor(Math.random() * (maximum - minimum)) + minimum;
+
+const createImageNode = () => {
+    const container = document.createElement("div");
+    container.className = "p-4";
+    const imagen = document.createElement("img");
+    imagen.className = "mx-auto";
+    imagen.width = "320";
+    imagen.src = `https://randomfox.ca/images/${random()}.jpg`;
+
+    container.appendChild(imagen);
+
+    return container;
+};
+
+const mountNode = document.getElementById("images");
+
+const myButton = document.querySelector("button");
+const addImage = () => {
+    const newImage = createImageNode();
+    mountNode.appendChild(newImage);
+    registerImage(newImage);
+};
+
+myButton.addEventListener("click", addImage);
